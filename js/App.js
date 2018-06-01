d8144c40a240a85e0b830b72f504c2481a214cc5
Pranay Ankit <pranayankit414@gmail.com>
2018-06-01 10:56:49 +0530

    refactored codebase

---
+++ js/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Homescreen from './components/HomeScreen/HomeScreen';
+
+export default class App1 extends Component {
+    render() {
+        return(
+            <Homescreen/>
+        );
+    }
+}
