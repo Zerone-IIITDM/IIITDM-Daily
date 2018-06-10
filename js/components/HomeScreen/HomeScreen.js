1d05f72a0fee827ae24638fc2ffe30abeca5cfad
Nipun Bharti <ced16i026@iiitdm.ac.in>
2018-06-10 19:33:21 +0530

    Added: Placeholder for college logo

--- js/components/HomeScreen/HomeScreen.js
+++ js/components/HomeScreen/HomeScreen.js
@@ -1,23 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text, Platform, StyleSheet } from 'react-native';
-
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' +
-        'Cmd+D or shake for dev menu',
-    android: 'Double tap R on your keyboard to reload,\n' +
-        'Shake or press menu button for dev menu',
-});
+import { View, Text, Platform, StyleSheet, Image } from 'react-native';
 
 export default class Homescreen extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Homescreen component goes here!
-                    </Text>
-                <Text style={styles.instructions}>
-                    {instructions}
-                </Text>
+                <View style={styles.instiLogo}>
+
+                </View>
             </View>
         );
     }
@@ -28,16 +18,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#F5FCFF',
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
+    instiLogo: {
+        flex:0.1,
+        width:75,
+        borderColor:'black',
+        borderWidth: 2
+    }
 });
